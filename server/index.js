2b0b6253b15a421606b7e25034badd67437caf42
enyichiaagu <enyichiaagu@gmail.com>
2024-05-18 10:56:30 +0100

    laying ground work for server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import 'dotenv/config';
+import { Octokit } from 'octokit';
 
 const app = express();
 
 const PORT = 3000;
+const { GITHUB_TEST_TOKEN } = process.env;
+
+const octokit = new Octokit({
+	auth: GITHUB_TEST_TOKEN,
+});
+
+// const repoInfo = await octokit.rest.repos.get({
+// 	owner: 'enyichiaagu',
+// 	repo: 'express-project',
+// });
+
+app.get('/api/:packageName', async (req, res) => {
+	const { packageName } = req.params;
+	res.send(repoInfo);
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
