e1fdb13b76cb18caf86270bff3d22f906af4d3a5
enyichiaagu <enyichiaagu@gmail.com>
2024-06-18 13:29:22 +0100

    removed octokit

--- server/controllers/commits.controller.js
+++ server/controllers/commits.controller.js
@@ -1,14 +1,5 @@
-import { Octokit } from 'octokit';
 import 'dotenv/config';
 
-const octokit = new Octokit({ auth: process.env.GITHUB_TEST_TOKEN });
-
-async function getDailyCommits(req, res) {
-  const commits = await octokit.rest.repos.getCommitActivityStats({
-    owner: 'facebook',
-    repo: 'react'
-  });
-  res.send({ data: commits.data.length });
-}
+async function getDailyCommits(req, res) {}
 
 export { getDailyCommits };
