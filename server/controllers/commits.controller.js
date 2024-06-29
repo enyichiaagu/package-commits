e32756c3298b42e01a78dc3fbe68e319203e2e79
enyichiaagu <enyichiaagu@gmail.com>
2024-06-29 08:03:26 +0100

    playing with my controllers

--- server/controllers/commits.controller.js
+++ server/controllers/commits.controller.js
@@ -1,19 +1,19 @@
 import 'dotenv/config';
 
 async function getDailyCommits(req, res) {
-  const { owner, repo } = req.commitParams;
+	const { owner, repo } = req.commitParams;
 
-  const fetchResponse = await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/commits?per_page=30`,
-    {
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${process.env.GITHUB_TEST_TOKEN}`
-      }
-    }
-  );
-  const commits = await fetchResponse.json();
+	const fetchResponse = await fetch(
+		`https://api.github.com/repos/${owner}/${repo}/commits?per_page=30`,
+		{
+			headers: {
+				'X-GitHub-Api-Version': '2022-11-28',
+				Accept: 'application/vnd.github+json',
+				Authorization: `Bearer ${process.env.GITHUB_TEST_TOKEN}`,
+			},
+		}
+	);
+	const commits = await fetchResponse.json();
 }
 
 export { getDailyCommits };
