179cd2c8780f779377f4731ff11cf8df0da1d888
enyichiaagu <enyichiaagu@gmail.com>
2024-07-25 17:06:25 +0100

    removed some files

--- server/routes/commits.router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDailyCommits } from '../controllers/commits.controller.js';
-
-const commitsRouter = express.Router();
-
-commitsRouter.get('/daily', getDailyCommits);
-
-export default commitsRouter;
