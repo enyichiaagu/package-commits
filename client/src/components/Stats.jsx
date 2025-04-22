23e719fa516c7b865224ac5d30fceb6c09c20a8b
enyichiaagu <enyichiaagu@gmail.com>
2025-04-22 18:01:39 +0100

    style(client): Updated unresolved issues grading

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -16,9 +16,9 @@ const Stats = memo(
             <>
               <span
                 className={`px-2 py-0.5 font-bold shadow-sm  ${
-                  data <= 5
+                  data <= 10
                     ? 'bg-green-100 text-green-700'
-                    : data <= 20
+                    : data <= 30
                     ? 'bg-yellow-100 text-yellow-600'
                     : 'bg-red-100 text-red-500'
                 }`}
