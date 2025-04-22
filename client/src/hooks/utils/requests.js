6824e565403d969018e5aa6f09734fec6191d24e
enyichiaagu <enyichiaagu@gmail.com>
2025-04-22 18:19:59 +0100

    chore(client): Deleting an unused file, 'requests.js'

--- client/src/hooks/utils/requests.js
+++
@@ -1,7 +0,0 @@
-const headers = {
-  Accept: 'application/vnd.github+json',
-  'X-GitHub-Api-Version': '2022-11-28',
-  // Authorization: `Bearer ${import.meta.env.VITE_GITHUB_PAT}`,
-};
-
-export { headers };
