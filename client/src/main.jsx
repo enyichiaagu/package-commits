757fd21e60d30c49aab91c0ac15f364b22dc401e
enyichiaagu <enyichiaagu@gmail.com>
2025-04-26 14:23:40 +0100

    fix(client): Properly import analytic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
