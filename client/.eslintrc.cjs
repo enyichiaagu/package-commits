c5b947d0969b5f4e72251566d643c5975a2d5a82
enyichiaagu <enyichiaagu@gmail.com>
2025-05-19 13:11:09 +0100

    deps(client): Removed the eslint effects plugin

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,13 +10,12 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'react-you-might-not-need-an-effect'],
+  plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react-you-might-not-need-an-effect/you-might-not-need-an-effect': 'warn',
   },
 };
