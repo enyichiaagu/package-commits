320353b7b4604ae9ed69ecf7d95081e932b29e9f
enyichiaagu <enyichiaagu@gmail.com>
2025-06-06 20:19:34 +0100

    fix: Migrating the project to TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    // reactRouter(),
+    react(),
+    tailwindcss(),
+  ],
 });
