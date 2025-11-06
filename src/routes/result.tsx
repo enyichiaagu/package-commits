a536480591dc23d30a20bb4bd674655f95c70fb1
A. Enyichi Agu <enyichiaagu@gmail.com>
2025-11-06 18:00:44 +0100

    fix, chore: updated some packages and results title

--- src/routes/result.tsx
+++ src/routes/result.tsx
@@ -1,77 +1,81 @@
-import { useState, useRef } from 'react';
-import { Link, useParams, useSearchParams } from 'react-router';
-import Header from '../components/Header';
-import SearchBox from '../components/SearchBox';
-import PackageTitle from '../components/PackageTitle';
-import GraphControls from '../components/GraphControls';
-import GraphBox from '../components/GraphBox';
-import Stats from '../components/Stats';
-import TokenDialog from '../components/TokenDialog';
-import usePackage from '../hooks/usePackage';
-import type { Period } from 'src/hooks/useCommits';
-import type { TabTypes } from 'src/components/ControlBtn';
+import { useState, useRef } from "react";
+import { Link, useParams, useSearchParams } from "react-router";
+import Header from "../components/Header";
+import SearchBox from "../components/SearchBox";
+import PackageTitle from "../components/PackageTitle";
+import GraphControls from "../components/GraphControls";
+import GraphBox from "../components/GraphBox";
+import Stats from "../components/Stats";
+import TokenDialog from "../components/TokenDialog";
+import usePackage from "../hooks/usePackage";
+import type { Period } from "src/hooks/useCommits";
+import type { TabTypes } from "src/components/ControlBtn";
 
 export function meta() {
-  return [{ title: 'Very Cool App2' }];
+	return [{ title: "Commits Graph" }];
 }
 
 export default function Result() {
-  const params = useParams();
-  const [searchParams] = useSearchParams();
-  const [period, setPeriod] = useState<Period | null>('Current');
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const refresh = useRef(null);
+	const params = useParams();
+	const [searchParams] = useSearchParams();
+	const [period, setPeriod] = useState<Period | null>("Current");
+	const [dialogOpen, setDialogOpen] = useState(false);
+	const refresh = useRef(null);
 
-  const { '*': pkg } = params;
-  const { pkgData, isLoading, error } = usePackage(pkg);
-  const tabType = (searchParams.get('tab') || 'daily') as TabTypes;
+	const { "*": pkg } = params;
+	const { pkgData, isLoading, error } = usePackage(pkg);
+	const tabType = (searchParams.get("tab") || "daily") as TabTypes;
 
-  return (
-    <>
-      <Header>
-        <Link to='/' className='order-1'>
-          <h1 className='font-bold text-2xl tracking-tight [word-spacing:-0.5rem] sm:flex-col sm:flex italic'>
-            <span>package</span>{' '}
-            <span className='sm:-mt-[0.9rem] relative sm:left-1.5'>
-              commits
-            </span>
-          </h1>
-        </Link>
-        <div className='flex items-center order-3 sm:order-2 col-span-full sm:col-span-1 sm:max-w-lg'>
-          <SearchBox variant='normal' pkg={pkg} />
-        </div>
-      </Header>
-      <main className='mt-5 sm:mt-10 mx-auto px-4 max-w-240'>
-        <>
-          {isLoading && 'Loading...'}
-          {!isLoading &&
-            (error ? error.message : <PackageTitle pkgData={pkgData} />)}
-        </>
-        {!error && (
-          <>
-            <GraphControls
-              tabType={tabType}
-              pkgData={pkgData}
-              setPeriod={setPeriod}
-              setDialogOpen={setDialogOpen}
-              ref={refresh}
-            />
-            <GraphBox
-              tabType={tabType}
-              pkgData={pkgData}
-              period={period}
-              setDialogOpen={setDialogOpen}
-              ref={refresh}
-            />
-            <Stats pkgData={pkgData} period={period} />
-          </>
-        )}
-      </main>
-      <TokenDialog
-        dialogOpen={dialogOpen}
-        setDialogOpen={setDialogOpen}
-        refresh={refresh}
-      />
-    </>
-  );
+	return (
+		<>
+			<Header>
+				<Link to='/' className='order-1'>
+					<h1 className='font-bold text-2xl tracking-tight [word-spacing:-0.5rem] sm:flex-col sm:flex italic'>
+						<span>package</span>{" "}
+						<span className='sm:-mt-[0.9rem] relative sm:left-1.5'>
+							commits
+						</span>
+					</h1>
+				</Link>
+				<div className='flex items-center order-3 sm:order-2 col-span-full sm:col-span-1 sm:max-w-lg'>
+					<SearchBox variant='normal' pkg={pkg} />
+				</div>
+			</Header>
+			<main className='mt-5 sm:mt-10 mx-auto px-4 max-w-240'>
+				<>
+					{isLoading && "Loading..."}
+					{!isLoading &&
+						(error ? (
+							error.message
+						) : (
+							<PackageTitle pkgData={pkgData} />
+						))}
+				</>
+				{!error && (
+					<>
+						<GraphControls
+							tabType={tabType}
+							pkgData={pkgData}
+							setPeriod={setPeriod}
+							setDialogOpen={setDialogOpen}
+							ref={refresh}
+						/>
+						<GraphBox
+							tabType={tabType}
+							pkgData={pkgData}
+							period={period}
+							setDialogOpen={setDialogOpen}
+							ref={refresh}
+						/>
+						<Stats pkgData={pkgData} period={period} />
+					</>
+				)}
+			</main>
+			<TokenDialog
+				dialogOpen={dialogOpen}
+				setDialogOpen={setDialogOpen}
+				refresh={refresh}
+			/>
+		</>
+	);
 }
